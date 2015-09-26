42b617664ffa91a443a6c0d40f93a50fab85b806
Sarah Foregger <sarahfo@gmail.com>
2015-09-26 14:52:07 -0700

    Edit object form added

--- djangoapp/collection/views.py
+++ djangoapp/collection/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from collection.models import Thing
+from collection.forms import ThingForm
 
 # Create your views here.
 def index(request):
@@ -9,7 +10,22 @@ def index(request):
 		})
 
 def thing_detail(request, slug):
-	thing = Things.objects.get(slug=slug)
+	thing = Thing.objects.get(slug=slug)
 	return render(request, 'things/thing_detail.html', {
 		'thing': thing,
+		})
+
+def edit_thing(request, slug):
+	thing = Thing.objects.get(slug=slug)
+	form_class = ThingForm
+	if request.method =='POST':
+		form = form_class(data=request.POST, instance=thing)
+		if form.is_valid():
+			form.save()
+			return redirect('thing_detail', slug=thing.slug)
+	else:
+		form = form_class(instance=thing)
+	return render(request, 'things/edit_thing.html', {
+		'thing': thing,
+		'form': form,
 		})
--- djangoapp/djangoapp/urls.py
+++ djangoapp/djangoapp/urls.py
@@ -22,6 +22,7 @@ urlpatterns = patterns('',
 	url(r'^about/$', TemplateView.as_view(template_name = 'about.html'),name='about'),
 	url(r'^contact/$', TemplateView.as_view(template_name = 'contact.html'),name='contact'),
     url(r'^things/(?P<slug>[-\w]+)/$', 'collection.views.thing_detail', name='thing_detail'),
+    url(r'^things/(?P<slug>[-\w]+)/edit/$', 'collection.views.edit_thing', name='edit_thing'),
     url(r'^admin/', include(admin.site.urls)),
 
 )
