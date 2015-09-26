b30c15278cc2953ca56ab56eca993006247d6968
Sarah Foregger <sarahfo@gmail.com>
2015-09-26 14:24:27 -0700

    created links on index page

--- djangoapp/djangoapp/urls.py
+++ djangoapp/djangoapp/urls.py
@@ -21,6 +21,7 @@ urlpatterns = patterns('',
 	url(r'^$', 'collection.views.index', name = 'home'),
 	url(r'^about/$', TemplateView.as_view(template_name = 'about.html'),name='about'),
 	url(r'^contact/$', TemplateView.as_view(template_name = 'contact.html'),name='contact'),
+    url(r'^things/(?P<slug>[-\w]+)/$', 'collection.views.thing_detail', name='thing_detail'),
     url(r'^admin/', include(admin.site.urls)),
 
 )
