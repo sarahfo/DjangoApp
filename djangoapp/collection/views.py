c4bb59b6523144946e5d1ca22e1b24a2e458dd81
Sarah Foregger <sarahfo@Sarahs-MacBook-Pro-2.local>
2015-09-26 13:30:01 -0700

    Added nav & urls

--- djangoapp/collection/views.py
+++ djangoapp/collection/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
 	#this is your new views
-	return render(request, 'index.html')
+	number = 6 
+	return render(request, 'index.html', {'number': number,
+		})
