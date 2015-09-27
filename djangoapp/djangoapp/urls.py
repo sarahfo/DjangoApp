ae27bd8c52ceecc0aefa876437fe06a8022537a4
Sarah Foregger <sarahfo@gmail.com>
2015-09-26 23:40:19 -0700

    Registration and Login

--- djangoapp/djangoapp/urls.py
+++ djangoapp/djangoapp/urls.py
@@ -23,6 +23,7 @@ urlpatterns = patterns('',
 	url(r'^contact/$', TemplateView.as_view(template_name = 'contact.html'),name='contact'),
     url(r'^things/(?P<slug>[-\w]+)/$', 'collection.views.thing_detail', name='thing_detail'),
     url(r'^things/(?P<slug>[-\w]+)/edit/$', 'collection.views.edit_thing', name='edit_thing'),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
 )
